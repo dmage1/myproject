851ba6a6060277732a134e278319f3219661b4f1
David Magee <dmage1@sky.com>
2018-10-23 21:12:19 +0100

    Added CustomerValidator

---
+++ src/main/java/com/example/myapplication/customer/CustomerValidator.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.customer;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class CustomerValidator implements Validator {
+
+    /**
+     * This Validator validates Customer instances, and any subclasses of Customer too
+     */
+    public boolean supports(Class clazz) {
+        return Customer.class.isAssignableFrom(clazz);
+    }
+
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "field.required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "field.required");
+    }
+}
